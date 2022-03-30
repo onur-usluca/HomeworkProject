fe7869751cc2ac41941ffefa99fe2ebad26fcca8
Onur Usluca <onur-usluca>
2022-03-29 21:00:27 -0400

    This is Onur first fork

---
+++ src/Onur/Test.java
@@ -0,0 +1,8 @@
+package Onur;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("I am learning GitHub");
+    }
+}
---
+++ src/homeworkproject/test.java
@@ -0,0 +1,9 @@
+package homeworkproject;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git");
+    }
+}
